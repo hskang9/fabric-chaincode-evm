b8eab69f4ab1d49dae9e3c5aa43cbafd8c8e4be8
Swetha Repakula <srepaku@us.ibm.com>
2018-04-10 16:51:09 -0700

    evmscc builds but not correctly

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,23 +6,25 @@ FABRIC_PATH=$1
 EVMSCC_PATH=$2
 
 echo "installing govendor"
-mkdir -p $GOPATH/bin
-curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
+go get -u github.com/kardianos/govendor
 
 
 cd $EVMSCC_PATH
 
-echo "removing packages already vendored in fabric from evmscc"
-comm -12 <(cd $EVMSCC_PATH && dep status| awk '{if(NR>1)print $1}' | sort) <(cd $FABRIC_PATH && dep status| awk '{if(NR>1)print $1}' | sort) | xargs -n1 -I{} rm -rf vendor/{} 
+# echo "removing packages already vendored in fabric from evmscc"
+# comm -12 <(cd $EVMSCC_PATH && govendor list +v | awk '{print $2}' | sort) <(cd $FABRIC_PATH && govendor list +v | awk '{print $2}' | sort) | xargs -n1 -I{} rm -rf vendor/{} 
 
 echo "copying packages needed by evmscc to fabric vendor"
 cp -r $EVMSCC_PATH/vendor/* $FABRIC_PATH/vendor/ #&& rm -rf $EVMSCC_PATH/vendor
 
+# echo "going to sleep for 5 min"
+# sleep 300
+
 echo "copying evmscc to fabric plugin"
 mkdir -p $FABRIC_PATH/plugin/evmscc && cp -r $EVMSCC_PATH/evmscc/evmscc.go $FABRIC_PATH/plugin/evmscc
-mkdir -p $FABRIC_PATH/vendor/github.com/hyperledger/fabric-chaincode-evm
-cp -r $EVMSCC_PATH/evmscc/mocks $FABRIC_PATH/vendor/github.com/hyperledger/fabric-chaincode-evm/.
-cp -r $EVMSCC_PATH/evmscc/statemanager $FABRIC_PATH/vendor/github.com/hyperledger/fabric-chaincode-evm/.
+mkdir -p $FABRIC_PATH/vendor/github.com/hyperledger/fabric-chaincode-evm/evmscc
+cp -r $EVMSCC_PATH/evmscc/mocks $FABRIC_PATH/vendor/github.com/hyperledger/fabric-chaincode-evm/evmscc/.
+cp -r $EVMSCC_PATH/evmscc/statemanager $FABRIC_PATH/vendor/github.com/hyperledger/fabric-chaincode-evm/evmscc/.
 
 echo "building evmscc plugin shared object to $LIB_DIR"
 cd $FABRIC_PATH/plugin && go build -o $LIB_DIR/evmscc.so -buildmode=plugin ./evmscc
